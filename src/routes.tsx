00930c570650d7721d7b198b9dc126a075918e8a
Bruna Barrón <barron.bruna@gmail.com>
2021-11-19 16:10:31 -0300

    feat: install styled components and create GlobalStyles

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
+import { GlobalStyles } from "./styles/GlobalStyle"
 
 const TheRoutes: React.FC = () => {
     return(
         <Router>
+            <GlobalStyles />
             <Routes>
                 <Route path="/" element={<Home />}>
                 </Route>
